2d0d6eb118796f2ab6bffdf7313e754870a55b8f
Thushar-Sreenivas <thusharsreenivas@gmail.com>
2024-02-17 11:40:45 +0530

    Update App.tsx and AppNavigator

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,23 @@
+// src/navigation/AppNavigator.tsx
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+
+export type RootStackParamList = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const AppNavigator: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
