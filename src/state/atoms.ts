2c19f5fd85cd7b1a2f73bff87b78da443e5d1f66
Thushar-Sreenivas <thusharsreenivas@gmail.com>
2024-02-17 13:08:43 +0530

    Add perist logic to save task

--- src/state/atoms.ts
+++ src/state/atoms.ts
@@ -1,8 +1,26 @@
 // src/state/tasksAtom.ts
 import {atom} from 'recoil';
 import {Task} from '../types';
+import {storage} from '../utils/storage';
+
+// Key for tasks data in storage
+const TASKS_STORAGE_KEY = 'tasks';
+
+// Try to load tasks from storage or default to empty array
+const defaultTasks = (() => {
+  const tasksJson = storage.getString(TASKS_STORAGE_KEY);
+  return tasksJson ? (JSON.parse(tasksJson) as Task[]) : [];
+})();
 
 export const tasksAtom = atom<Task[]>({
   key: 'tasksAtom',
-  default: [],
+  default: defaultTasks,
+  effects_UNSTABLE: [
+    ({onSet}) => {
+      onSet(tasks => {
+        // Serialize and save tasks whenever they change
+        storage.set(TASKS_STORAGE_KEY, JSON.stringify(tasks));
+      });
+    },
+  ],
 });
